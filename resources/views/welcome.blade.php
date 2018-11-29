4a677ae0aaf991e436b4674fe303420092898e3e
USER-20160304GT\Administrator <yitichen22@gmail.com>
2018-11-29 11:57:33 +0800

    '字符串太长'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel-立志做一个棒棒的博客
                 </div>
 
                 <div class="links">
