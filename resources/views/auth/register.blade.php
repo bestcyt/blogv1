ccbeb82d880e66c42e69ec65cc4def16f4178ffd
USER-20160304GT\Administrator <yitichen22@gmail.com>
2019-03-07 11:03:37 +0800

    'asdf'

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,11 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                {{--<div class="panel-heading">Register</div>--}}
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+                        {{--{{ csrf_field() }}--}}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
